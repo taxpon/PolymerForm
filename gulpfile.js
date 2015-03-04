b95ad0735e6371f401088a47ed5d144cbc139f60
Takuro Wada <taxpon@gmail.com>
2015-03-04 14:32:44 +0900

    Add example file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,57 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var pleeease = require('gulp-pleeease');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+browserSync({
+    server: './example'
+});
 
 gulp.task('uglify', function(){
     gulp.src('src/*.js')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(rename('jquery.polymer-form.min.js'))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('example/js'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('sass', function(){
+    gulp.src('src/*.scss')
+        .pipe(plumber())
+        .pipe(sass({style:'compressed'}))
+        .pipe(pleeease())
+        .pipe(rename('jquery.polymer-form.min.css'))
+        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('example/css'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('example', function(){
+    gulp.src('example/index.html')
+        .pipe(reload({stream:true}));
+
+});
+
+gulp.task('example-sass', function(){
+    gulp.src('example/css/style.scss')
+        .pipe(plumber())
+        .pipe(sass({style:'compressed'}))
+        .pipe(pleeease())
+        .pipe(gulp.dest('example/css'))
+        .pipe(reload({stream:true}));
 });
 
 gulp.task('watch', function(){
     gulp.watch('src/*.js', ['uglify']);
+    gulp.watch('src/*.scss', ['sass']);
+    gulp.watch('example/index.html', ['example']);
+    gulp.watch('example/css/style.scss', ['example-sass']);
 });
 
 gulp.task('deafault', ['watch'], function(){
--- src/jquery.polymer-form.js
+++ src/jquery.polymer-form.js
@@ -6,18 +6,30 @@
 
     $.fn.elasticForm = function(options){
         var setting = $.extend({
-
+            base_color: "#AAAAAA",
+            active_color: "#DDDDDD",
+            bar_height: "2px"
         }, options);
 
         this.each(function(){
-            var hoge = 120;
             var thiz = $(this);
-            var label_name = $(this).attr("label");
+            var label_name = $(this).attr('label');
+            var base_color = $(this).attr('base_color') || setting.base_color;
+            var active_color = $(this).attr('active_color') || setting.active_color;
+            var bar_height = $(this).attr('bar_height') || setting.bar_height;
 
-            var dom_label = $("<label></label>").text(label_name);
+            var $dom_label = $("<label></label>").text(label_name);
+            var $div_bar = $("<div class='bar'></div>");
+            var $div_bar_in = $("<div class='bar-in'></div>");
+
+            $div_bar.css({backgroundColor: base_color, height: bar_height});
+            $div_bar_in.css({backgroundColor: active_color, height: bar_height});
+            $div_bar.append($div_bar_in);
+
             $(this).wrap("<div class='elastic-form'>");
-            $(this).after(dom_label);
-            $(this).after("<div class='bar'><div class='bar-in'></div></div>");
+            $(this).after($dom_label);
+            $(this).after($div_bar);
+
 
             $(this).focus(function(){
                 var bar = $(this).parent().find(".bar-in");
@@ -31,7 +43,7 @@
                     $(this).parent().removeClass("dirty");
                 }
             });
-            dom_label.click(function(){
+            $dom_label.click(function(){
                 thiz.focus();
             });
         });
